431d7d6bfe1f43b0cba0e310980713cfd7886405
Joe Jenne <33004635+jhjn@users.noreply.github.com>
2025-10-07 21:23:32 +0100

    Pull out the md parser

--- wiki.go
+++ wiki.go
@@ -19,6 +19,14 @@ import (
 	"github.com/yuin/goldmark/renderer/html"
 )
 
+// Markdown parser: GFM + ::: fences + {.foo} attrs
+// NOTE: In future add https://github.com/yuin/goldmark-highlighting
+var md = goldmark.New(
+	goldmark.WithExtensions(extension.GFM, &fences.Extender{}),
+	goldmark.WithParserOptions(parser.WithAttribute()),
+	goldmark.WithRendererOptions(html.WithUnsafe()),
+)
+
 // A parsed wiki page.
 // Used to serve HTML and understand inter-page linking.
 type Page struct {
@@ -26,10 +34,10 @@ type Page struct {
 	Name string // filename relative to wiki dir without .md
 	Raw  string // raw markdown
 	// Filled after parsing
-	Title     string          // from first '#' heading or Name
+	Title     string          // from the first '#' heading else Name
 	HTML      template.HTML   // The converted markdown
-	Links     map[string]bool // set of outbound page names
-	Backlinks []string        // inbound page names
+	Links     map[string]bool // set of outbound wiki-linked page names
+	Backlinks []string        // inbound wiki-linked page names
 }
 
 // A collection of parsed markdown pages.
@@ -79,8 +87,86 @@ func sortBacklinks(a, b string) int {
 	return 0 // Should never reach here
 }
 
+// Update page objects to create backlinks from scratch.
+func buildBacklinks(pages map[string]*Page) {
+	pageLinkers := map[string]map[string]struct{}{}
+	for name := range pages {
+		pageLinkers[name] = map[string]struct{}{}
+	}
+
+	// Build set of pages each with set of pages that link to it
+	for linker, p := range pages {
+		for target := range p.Links {
+			if _, ok := pages[target]; ok {
+				pageLinkers[target][linker] = struct{}{}
+			}
+		}
+		// Every page implicitly links to 'search'
+		pageLinkers["search"][linker] = struct{}{}
+	}
+
+	// Construct backlinks array for each page
+	for name, linkers := range pageLinkers {
+		backlinks := []string{}
+		for linker := range linkers {
+			backlinks = append(backlinks, linker)
+		}
+		pages[name].Backlinks = backlinks
+		slices.SortFunc(pages[name].Backlinks, sortBacklinks)
+	}
+}
+
+// Only call for files ending in .md
+func loadPage(path string) (*Page, error) {
+	name := strings.TrimSuffix(path, ".md")
+
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	p := &Page{
+		Name:  name,
+		Raw:   string(b),
+		Links: map[string]bool{},
+	}
+
+	// Process title (if '# ' get string until newline)
+	if strings.HasPrefix(p.Raw, "# ") && strings.Index(p.Raw, "\n") > 0 {
+		p.Title = strings.TrimSpace(p.Raw[2:strings.Index(p.Raw, "\n")])
+	}
+
+	// Process wikilinks
+	processed := linkRe.ReplaceAllStringFunc(p.Raw, func(m string) string {
+		sub := linkRe.FindStringSubmatch(m)
+		if len(sub) >= 2 {
+			target := strings.TrimSpace(sub[1])
+			p.Links[target] = true // Add link to page set
+
+			var label string
+			if len(sub) >= 3 {
+				label = strings.TrimSpace(sub[2])
+			}
+			if label == "" {
+				label = target
+			}
+			return fmt.Sprintf("[%s](%s)", label, target)
+		}
+		return m // Match but not right size... empty [[]]?
+	})
+
+	// Render HTML
+	var sb strings.Builder
+	if err := md.Convert([]byte(processed), &sb); err != nil {
+		return nil, err
+	}
+	p.HTML = template.HTML(sb.String())
+
+	return p, nil
+}
+
 // Create page data from a directory
-func getPages(dir string) (map[string]*Page, error) {
+func loadPages(dir string) (map[string]*Page, error) {
 	pages := map[string]*Page{}
 
 	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
@@ -115,14 +201,6 @@ func getPages(dir string) (map[string]*Page, error) {
 		}
 	}
 
-	// First pass: extract outbound links and render HTML
-	// NOTE: In future add https://github.com/yuin/goldmark-highlighting
-	md := goldmark.New(
-		goldmark.WithExtensions(extension.GFM, &fences.Extender{}),
-		goldmark.WithParserOptions(parser.WithAttribute()),
-		goldmark.WithRendererOptions(html.WithUnsafe()),
-	)
-
 	for _, p := range pages {
 		// Process title (if '# ' get string until newline)
 		if strings.HasPrefix(p.Raw, "# ") && strings.Index(p.Raw, "\n") > 0 {
@@ -156,27 +234,8 @@ func getPages(dir string) (map[string]*Page, error) {
 	}
 
 	// Build backlinks
-	pageLinkers := map[string]map[string]struct{}{}
-	for name := range pages {
-		pageLinkers[name] = map[string]struct{}{}
-	}
-	for linker, p := range pages {
-		for target := range p.Links {
-			if _, ok := pages[target]; ok {
-				pageLinkers[target][linker] = struct{}{}
-			}
-		}
-		// Every page implicitly links to 'search'
-		pageLinkers["search"][linker] = struct{}{}
-	}
-	for name, mset := range pageLinkers {
-		arr := []string{}
-		for k := range mset {
-			arr = append(arr, k)
-		}
-		pages[name].Backlinks = arr
-		slices.SortFunc(pages[name].Backlinks, sortBacklinks)
-	}
+	buildBacklinks(pages)
+
 	return pages, nil
 }
 
@@ -184,7 +243,7 @@ func getPages(dir string) (map[string]*Page, error) {
 // NOTE: Later handle updating the template if it changes.
 // NOTE: Implement the updating of single files!
 func (w *Wiki) Update() error {
-	pages, err := getPages(w.Dir)
+	pages, err := loadPages(w.Dir)
 	if err != nil {
 		return err
 	}
